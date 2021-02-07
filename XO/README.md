c27c91bbdc191c7cf35a52b201fe7eadf494caa1
walczyk123 <walczyk.kamil@gmail.com>
2021-02-07 19:49:07 +0100

    long-awaited third update

--- XO/README.md
+++ XO/README.md
@@ -1,7 +1,18 @@
 # Gra w kółko-krzyżyk X0
 
-Jest to repozytorium, zawierające pierwszy "poważniejszy" projekt jakim jest napisanie gry w kółko krzyżyk w języku Ruby.  
+Jest to pierwszy "poważniejszy" projekt, jakim jest napisanie gry w kółko krzyżyk w języku Ruby.  
 Przewiduje 3 tryby gry:  
 -> Gracz vs Gracz,   
 -> Gracz vs Komputer,   
--> Komputer vs Komputer   
+-> Komputer vs Komputer  
+
+Uwagi:  
+- Skupic sie na szkielecie  
+- Generowac mape a nie rysowac na pale  
+- nie przekazywac wyzej jakis gowien  
+- byc moze uda sie napisac osobno klase planszy, osobno graczy i spiąć to w grze  
+  
+Uwagi 07.02
+- co do kodu, popytac przy komentarzach
+- ogarnac powtorzenie ruchu jezeli ten był zły, np. wspolrzedna to litera
+- wymyslec sprawdzanie w lini
--- XO/game_main.rb
+++ XO/game_main.rb
@@ -1,15 +1,12 @@
-
 class XO
-
   def start
-    gamemode=say_hello()
+    gamemode = say_hello()
     #puts(gamemode) #tymczasowo to tu wisi do debuggowania
-    zjebales_gre() if gamemode==4 # tez tymczasowe rozwiazanie xd
+    zjebales_gre() if gamemode == 4 # tez tymczasowe rozwiazanie xd
 
-    pvp() if gamemode==1
-    pve() if gamemode==2
-    eve() if gamemode==3
-
+    pvp() if gamemode == 1
+    pve() if gamemode == 2
+    eve() if gamemode == 3
   end
 
   def say_hello
@@ -18,7 +15,7 @@ class XO
     puts "Do you want to play a game? [y / n]"
     prompt = STDIN.gets.chomp
     return unless prompt == 'y'
-    mode=eager()
+    mode = eager()
     #unless prompt == 'n' ? return zjebales_gre() , mode=eager() end
   end
 
@@ -36,62 +33,92 @@ class XO
   end
 
   def pvp()
-    puts "AAAAA ROZRUBA"
+    puts "Let's fight"
+    board = Matrix.build(3) { " " }
 
-    # puts board.at(1)
-    board=Matrix.build(3) { " " }
-    p1 = PLAYER.new(1,:x,board)
-    p2 = PLAYER.new(2,:o,board)
+    until check
+      p1 = PLAYER.new(1, "x", board) # pasowalo by symbole, chwilowo nad tym walcze
+      p2 = PLAYER.new(2, "o", board)
+    end
   end
+
   def pve
     puts "wiec wybrales smierc, rozERWE CIE NA KAWałki"
   end
+
   def eve
     puts "I rzekl mędrzec do kutasów - walczcie, kto wygra, czeka go nagroda"
   end
+
   def zjebales_gre
     puts "miales tylko jedno robote"
   end
 
-
-
   def check
-    return false
+    false
   end
 
 end
 
 class PLAYER
-  def initialize(id,sign,board)
+  def initialize(id, sign, board)
     @pl_id = id
     @pl_sign = sign
     @board = board
 
     show
-    ins=insert
-
-    puts "wrong char!" if ins == 0
-    puts "number is out of boundary!" if ins == 1
-    puts "spot has been already taken!" if ins == 2
+    insert(sign)
   end
 
   def show
     puts "Your turn Player #{@pl_id}, type x,y: "
     puts "   │ 1 │ 2 │ 3 │"
     puts "───┼───┼───┼───┤"
-    puts " 1 │ #{@board[0,0]} │ #{@board[0,1]} │ #{@board[0,2]} │"
+    puts " 1 │ #{@board[0, 0]} │ #{@board[0, 1]} │ #{@board[0, 2]} │"
     puts "───┼───┼───┼───┤"
-    puts " 2 │ #{@board[1,0]} │ #{@board[1,1]} │ #{@board[1,2]} │"
+    puts " 2 │ #{@board[1, 0]} │ #{@board[1, 1]} │ #{@board[1, 2]} │"
     puts "───┼───┼───┼───┤"
-    puts " 3 │ #{@board[2,0]} │ #{@board[2,1]} │ #{@board[2,2]} │"
+    puts " 3 │ #{@board[2, 0]} │ #{@board[2, 1]} │ #{@board[2, 2]} │"
   end
-  def insert
+
+  def insert(sign)
     place = gets.chomp
     cords = place.split(",")
-    puts "X: #{cords[0]}, Y: #{cords[1]}"
-    return 0 until num?(cords[0]) && num?(cords[1])
-    return 1 until good_num?(cords[0]) && good_num?(cords[1])
-    # return 2 until empty?(cords)
+    cords_int = cords.map(&:to_i)
+    puts "X: #{Integer(cords[0])}, Y: #{cords[1]}"
+
+    # z jakiegos powodu nie dziala, zapytać!
+    # update chyba naprawilem
+    # update 2 chyba nie
+    #good?(cords,cords_int) ? (return true) : (return false)
+
+    if good?(cords)
+      @board[cords_int[0], cords_int[1]] = sign
+    else
+
+    end
+    puts " w8"
+  end
+
+  def good?(cords)
+    #tu mi sie nie podobaja ify i trzeba pogadac z Radkiem:
+    # 1. za duzo zbyt drobnych moim zdaniem
+    # 2. if !true sie jakos zamienialo unless czy tam until (zapytac) update: zrobione przy empty?
+    # 3. moze da sie to wyciagnac calkiem w inne miejsce
+    if !num?(cords[0]) && !num?(cords[1])
+      puts "wrong char!"
+      false
+    end
+    if !good_num?(cords[0]) && !good_num?(cords[1])
+      puts "number is out of boundary!"
+      false
+    end
+    cords_int = cords.map(&:to_i)
+    unless empty?(cords_int)
+      puts "spot has been already taken!"
+      false
+    end
+      true
   end
 
   def num?(str)
@@ -101,16 +128,14 @@ class PLAYER
   end
 
   def good_num?(str)
-    Integer(str) == 1 || Integer(str) == 2 || Integer(str) == 3
+    Integer(str) == 1 || Integer(str) == 2 || Integer(str) == 0
   end
 
   def empty?(cords)
-    x=cords[0]
-    y=cords[1]
-    true if @board[x,y].include?(:x)
+    x = cords[0]
+    y = cords[1]
+    true if @board[x, y] == " "
   end
-
-
 end
 
 require 'matrix'
